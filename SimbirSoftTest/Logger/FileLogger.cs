f82c67b2bd512e7c9f234bb9f1c7b43d1cc04069
codmonkdev <codemonkeydeveloper@mail.ru>
2021-04-07 19:33:15 +0400

    add logger Chain of responsibility

---
+++ SimbirSoftTest/Logger/FileLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SimbirSoftTest.Logger
+{
+    class FileLogger : ALogger
+    {
+        protected string filePathName;
+        public FileLogger(int mask,string filePathName)
+        {
+            this.mask = mask;
+            this.filePathName = filePathName;
+        }
+        protected override void writeMessage(string msg)
+        {
+            if (File.Exists(filePathName))
+            {
+                File.Delete(filePathName);
+            }
+            using (StreamWriter sw = File.CreateText(filePathName))
+            {
+                sw.WriteLine(msg);
+            }
+        }
+    }
+}
---
+++ SimbirSoftTest/Logger/LogBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SimbirSoftTest.Logger
+{
+    public class LogBuilder
+    {
+        static public ALogger buildLog(int mask)
+        {
+            ALogger aLogger, aLogger1;
+            aLogger = new ConsoleLogger(mask);
+            aLogger1 = new FileLogger(mask,Path.Combine(AppContext.BaseDirectory, "Log.log"));
+            aLogger.setNext(aLogger1);
+            return aLogger;
+
+        }
+    }
+}
