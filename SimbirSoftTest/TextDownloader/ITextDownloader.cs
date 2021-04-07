bcfdbf73ae2f4c2e38e60e9f5148b7af70b2ee2f
codmonkdev <codemonkeydeveloper@mail.ru>
2021-04-07 22:58:11 +0400

    added interface for text downloadText added simple text downloader via Agility

---
+++ SimbirSoftTest/TextDownloader/ITextDownloader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimbirSoftTest.TextDownloader
+{
+    public interface ITextDownloader
+    {
+        public string downloadText(string url);
+    }
+}
